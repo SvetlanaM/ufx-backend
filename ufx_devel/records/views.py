0c3b75ab1283c1766443828af3ce4e8c30bfb5e4
SvetlanaM <svetlana@margetova.eu>
2017-11-15 15:36:23 +0100

    Update for employees

--- ufx_devel/records/views.py
+++ ufx_devel/records/views.py
@@ -6,15 +6,7 @@ from .models import Record
 from rest_framework.parsers import MultiPartParser, FormParser
 
 class RecordCreateAPIView(generics.CreateAPIView):
+    serializer_class = RecordSerializer
     parser_classes = (MultiPartParser, FormParser,)
     queryset = Record.objects.all()
-    serializer_class = RecordSerializer
-
-    def post(self, request, format=None):
-        my_file = request.FILES['upload_to']
-        filename = '/tmp/myfile'
-        with open(filename, 'wb+') as temp_file:
-            for chunk in my_file.chunks():
-                temp_file.write(chunk)
-
-        my_saved_file = open(filename) #there you go
+    
